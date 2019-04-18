297d733c34e0bf54d76b67d5df1ccee763ec1158
林少聪 < @PC-20180515PRDG>
2019-04-18 13:40:18 +0800

    时限修改

--- src/main/java/com/anbang/qipai/chayuanshuangkou/msg/receiver/GameRoomMsgReceiver.java
+++ src/main/java/com/anbang/qipai/chayuanshuangkou/msg/receiver/GameRoomMsgReceiver.java
@@ -9,9 +9,17 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 
 import com.anbang.qipai.chayuanshuangkou.cqrs.c.domain.PukeGameValueObject;
 import com.anbang.qipai.chayuanshuangkou.cqrs.c.service.GameCmdService;
+import com.anbang.qipai.chayuanshuangkou.cqrs.q.dbo.JuResultDbo;
+import com.anbang.qipai.chayuanshuangkou.cqrs.q.dbo.PukeGameDbo;
+import com.anbang.qipai.chayuanshuangkou.cqrs.q.dbo.PukeGamePlayerDbo;
 import com.anbang.qipai.chayuanshuangkou.cqrs.q.service.PukeGameQueryService;
+import com.anbang.qipai.chayuanshuangkou.cqrs.q.service.PukePlayQueryService;
 import com.anbang.qipai.chayuanshuangkou.msg.channel.GameRoomSink;
 import com.anbang.qipai.chayuanshuangkou.msg.msjobj.CommonMO;
+import com.anbang.qipai.chayuanshuangkou.msg.msjobj.PukeHistoricalJuResult;
+import com.anbang.qipai.chayuanshuangkou.msg.service.ChayuanShuangkouGameMsgService;
+import com.anbang.qipai.chayuanshuangkou.msg.service.ChayuanShuangkouResultMsgService;
+import com.dml.mpgame.game.player.GamePlayerOnlineState;
 import com.google.gson.Gson;
 
 @EnableBinding(GameRoomSink.class)
@@ -23,6 +31,15 @@ public class GameRoomMsgReceiver {
 	@Autowired
 	private PukeGameQueryService pukeGameQueryService;
 
+	@Autowired
+	private PukePlayQueryService pukePlayQueryService;
+
+	@Autowired
+	private ChayuanShuangkouResultMsgService chayuanShuangkouResultMsgService;
+
+	@Autowired
+	private ChayuanShuangkouGameMsgService chayuanShuangkouGameMsgService;
+
 	private Gson gson = new Gson();
 
 	@StreamListener(GameRoomSink.WENZHOUSHUANGKOUGAMEROOM)
@@ -32,11 +49,26 @@ public class GameRoomMsgReceiver {
 		if ("gameIds".equals(msg)) {
 			List<String> gameIds = gson.fromJson(json, ArrayList.class);
 			for (String gameId : gameIds) {
-				PukeGameValueObject gameValueObject;
 				try {
-					gameValueObject = gameCmdService.finishGameImmediately(gameId);
-					pukeGameQueryService.finishGameImmediately(gameValueObject);
-				} catch (Exception e) {
+					PukeGameDbo pukeGameDbo = pukeGameQueryService.findPukeGameDboById(gameId);
+					boolean playerOnline = false;
+					for (PukeGamePlayerDbo player : pukeGameDbo.getPlayers()) {
+						if (GamePlayerOnlineState.online.equals(player.getOnlineState())) {
+							playerOnline = true;
+						}
+					}
+					if (playerOnline) {
+						chayuanShuangkouGameMsgService.delay(gameId);
+					} else {
+						PukeGameValueObject gameValueObject = gameCmdService.finishGameImmediately(gameId);
+						pukeGameQueryService.finishGameImmediately(gameValueObject);
+						JuResultDbo juResultDbo = pukePlayQueryService.findJuResultDbo(gameId);
+						PukeHistoricalJuResult juResult = new PukeHistoricalJuResult(juResultDbo, pukeGameDbo);
+						chayuanShuangkouResultMsgService.recordJuResult(juResult);
+						chayuanShuangkouGameMsgService.gameFinished(gameId);
+					}
+				} catch (Throwable e) {
+					e.printStackTrace();
 				}
 			}
 		}
--- src/main/java/com/anbang/qipai/chayuanshuangkou/msg/service/ChayuanShuangkouGameMsgService.java
+++ src/main/java/com/anbang/qipai/chayuanshuangkou/msg/service/ChayuanShuangkouGameMsgService.java
@@ -81,4 +81,13 @@ public class ChayuanShuangkouGameMsgService {
 		mo.setData(data);
 		chayuanShuangkouGameSource.chayuanShuangkouGame().send(MessageBuilder.withPayload(mo).build());
 	}
+
+	public void delay(String gameId) {
+		CommonMO mo = new CommonMO();
+		mo.setMsg("game delay");
+		Map data = new HashMap();
+		data.put("gameId", gameId);
+		mo.setData(data);
+		chayuanShuangkouGameSource.chayuanShuangkouGame().send(MessageBuilder.withPayload(mo).build());
+	}
 }
